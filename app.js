c791e101b4fc296b15d7fe78e3ddde2e0d6db896
ranjith reynold <ranjith reynold>
2017-09-11 18:15:08 +0530

    initial update

---
+++ app.js
@@ -0,0 +1,37 @@
+var express=require("express");
+var app=express();
+var bodyparser=require("body-parser");
+var http=require('http').Server(app);
+var io=require('socket.io')(http);
+
+function notify(req,res,next)
+{
+    console.log("request been made"+req.url);
+    next();
+}
+
+app.use(notify);
+app.use(bodyparser.json({limit: '50mb'}));
+app.use(bodyparser.urlencoded({limit: '50mb', extended: true}));
+
+
+io.on("connection",function(socket)
+{
+    console.log("A user connected:" + socket.id);
+
+
+    socket.on("message_full",function(){
+        io.sockets.emit("notify",{message:"groceryis full"});
+    })
+
+
+    socket.on("message_low",function(){
+        io.sockets.emit("notify",{message:"groceryis level is low"});
+    })
+});
+
+
+
+var server=app.listen(process.env.PORT || 5000,function(){
+    console.log("server running in port "+(process.env.PORT || 5000));
+});
