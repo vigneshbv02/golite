cfcece9f43ee8acef025837cb8f6916561056d81
ranjith reynold <ranjith reynold>
2017-09-21 11:01:04 +0530

    socket change

--- app.js
+++ app.js
@@ -5,90 +5,100 @@ var bodyparser=require("body-parser");
 var http=require('http').Server(app);
 var io=require('socket.io')(http);
 
+
+
+io.on("connection",function(socket){
+console.log("A user connected:"+socket.id);
+socket.emit('notify',{'message':socket.id});
+
+})
+
+
 /*var mqtt=require('mqtt');
-var broker=require('mosca');
+ var broker=require('mosca');
 
-var settings={
-    port:1883
-};
+ var settings={
+ port:1883
+ };
 
-var s=new broker.Server(settings);
-
-
-s.on('ready',function(){
-    console.log("Broker started in port 1883");
-});
-
-s.on('clientConnected',function(client){
-    console.log('client connected',client.id);
-
-});
-s.on('subscribed',function(topic,client){
-    console.log('subscibed:'+topic+' '+client.id);
-})
+ var s=new broker.Server(settings);
 
 
-s.on('published',function(data){
-    console.log('published:'+data.payload.toString()+' ');
-})
+ s.on('ready',function(){
+ console.log("Broker started in port 1883");
+ });
+
+ s.on('clientConnected',function(client){
+ console.log('client connected',client.id);
+
+ });
+ s.on('subscribed',function(topic,client){
+ console.log('subscibed:'+topic+' '+client.id);
+ })
+
+
+ s.on('published',function(data){
+ console.log('published:'+data.payload.toString()+' ');
+ })
 
 
 
-s.on('unsubscribed',function(topic,client){
-    console.log('unsubscibed:'+topic+' '+client.id);
-})
+ s.on('unsubscribed',function(topic,client){
+ console.log('unsubscibed:'+topic+' '+client.id);
+ })
 
-s.on('clientDisconnected',function(client){
-    console.log('clientDisconnected:'+client.id);
-
-
-});
+ s.on('clientDisconnected',function(client){
+ console.log('clientDisconnected:'+client.id);
 
 
-var option={
-    keepalive:10,
-    clientId:'coordinator',
-    port:1883,
-    host:'http://golite.herokuapp.com',
-    will: {
-        topic: 'WillMsg',
-        payload: 'Connection Closed abnormally..!',
-        qos: 0,
-        retain: false
-    }
-}
-
-var client=mqtt.connect('tcp://localhost:1883',option);
+ });
 
 
-client.on("connect",function(){
+ var option={
+ keepalive:10,
+ clientId:'coordinator',
+ port:1883,
+ host:'http://golite.herokuapp.com',
+ will: {
+ topic: 'WillMsg',
+ payload: 'Connection Closed abnormally..!',
+ qos: 0,
+ retain: false
+ }
+ }
 
-    setInterval(function(){
-     client.publish('intopic','hi every one',function(){
-     console.log("published");
-     });
-
-     },10000);
+ var client=mqtt.connect('tcp://localhost:1883',option);
 
 
+ client.on("connect",function(){
+
+ setInterval(function(){
+ client.publish('intopic','hi every one',function(){
+ console.log("published");
+ });
+
+ },10000);
 
 
 
 
-    client.on('message',function(topic,message){
-
-        console.log("{Coordinator} "+topic+"sending :"+message);
 
 
-        //socket.emit("rasp", {message: message.toString()});   //socket
-        io.to('room-golite').emit('notify',{messge:message.toString()});
-    })
+ client.on('message',function(topic,message){
+
+ console.log("{Coordinator} "+topic+"sending :"+message);
 
 
-});
+ //socket.emit("rasp", {message: message.toString()});   //socket
+ io.to('room-golite').emit('notify',{messge:message.toString()});
+ })
 
 
-*/
+ });
+
+
+ */
+
 
 
 
@@ -117,6 +127,15 @@ io.on("connection",function(socket)
 app.get('/',function(req,res){
     res.send("<marquee>GoLite is online By vivek and his team</marquee>");
 });
+app.get('/high',function(req,res){
+    console.log("high");
+    res.send("done");
+})
+
+app.get('/low',function(req,res){
+    console.log("low");
+    res.send("done");
+})
 
 
 var server=app.listen(process.env.PORT || 5000,function(){
